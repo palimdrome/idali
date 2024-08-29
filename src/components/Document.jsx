cb731c4b5072db6a86ffc4cad65f0050045b7b8a
palimdrome <limuelle.dca@gmail.com>
2024-08-29 15:21:58 +0800

    Added function to layout images on the document

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -6,11 +6,21 @@ function Document() {
 
     const docSize = "a4";
     const docOrientation = "portrait";
-    const imgSrcs = [
+    const imgSrcs1x1 = [
         "src/assets/image-1.JPG",
-        "src/assets/image-2.JPG",
-        "src/assets/image-3.JPG",
-        "src/assets/image-3.JPG"
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG"
+    ]
+    const imgSrcs2x2 = [
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG",
+        "src/assets/image-1.JPG"
     ]
 
     const exportToPDF = () => {
@@ -26,23 +36,32 @@ function Document() {
             pdf.save('images.pdf');
         });
     };
-
     return ( 
-        <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4 hidden'>
-            <div id='document' className='relative bg-white shadow-lg' style={{width: '210mm', height: '297mm'}}>
-                {imgSrcs.map((src, index) => (
+        <div className='relative min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4'>
+            <div id='document' className='flex flex-wrap content-start bg-white shadow-lg p-6' style={{width: '210mm', height: '297mm'}}>
+                {imgSrcs2x2.map((src, index) => (
                     <img 
                         key={index}
                         src={src}
                         alt={`2x2 ID ${index + 1}`}
-                        className='absolute border border-black'
-                        style={{width: '48mm',
-                                height: '48mm',
-                                left: `${index *48}mm`,
-                                top: '10mm'
-                            }}
+                        className='border border-black'
+                        style={{width: '48mm', height: '48mm'}}
                     />
                 ))}
+                <div
+                    className='flex flex-wrap content-start'
+                    style={{width: `${Math.ceil(imgSrcs1x1.length / 4) * 48}mm`, height: '48mm'}}
+                >
+                    {imgSrcs1x1.map((src, index) => (
+                        <img
+                            key={index}
+                            src={src}
+                            alt={`1x1 ID ${index + 1}`}
+                            className='border border-black'
+                            style={{width: '24mm', height: '24mm'}}
+                        />
+                    ))}
+                </div>
             </div>
 
             <button onClick={exportToPDF} className='mt-6 bg-blue-500 text-white px-4 py-2 rounded shadow'>
