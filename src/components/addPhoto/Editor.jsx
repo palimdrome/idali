86f9e13af34dea594f5e621f661fea9d1eb11bd7
Jon Robien Jinon <robien.jinon@gmail.com>
2024-09-03 11:24:19 +0800

    Updated UI to add multiple images; Added file type limit to file input

--- src/components/addPhoto/Editor.jsx
+++ src/components/addPhoto/Editor.jsx
@@ -1,31 +1,68 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PhotoDetails from './PhotoDetails';
 import { Button } from 'flowbite-react';
-import { MdAdd } from "react-icons/md";
+import { MdAdd } from 'react-icons/md';
 
-function AddPhotoButton({ count }) {
-    return (
-        <Button color='light' className={`w-full ${count > 0? 'lg:w-3/5 shadow-lg': 'hidden'}`}>
-            <MdAdd className="mr-2 h-5 w-5" />
-            ADD A PHOTO
-        </Button>
-    )
+function AddPhotoButton({ onAddPhoto, disabled }) {
+  return (
+    <Button
+      color="light"
+      className="w-full lg:w-3/5 shadow-lg"
+      onClick={onAddPhoto}
+      disabled={disabled}
+    >
+      <MdAdd className="mr-2 h-5 w-5" />
+      ADD A PHOTO
+    </Button>
+  );
 }
 
 function Editor() {
-    const [count, setCount] = useState(0);
+  const [photoDetailsList, setPhotoDetailsList] = useState([
+    { id: Date.now(), hasFile: false },
+  ]);
 
-    useEffect(() => {
-        console.log(`Count has changed to: ${count}`);
-    }, [count]);
+  // Function to add a new PhotoDetails component
+  const handleAddPhoto = () => {
+    const lastPhoto = photoDetailsList[photoDetailsList.length - 1];
+    if (lastPhoto.hasFile) {
+      const newPhoto = {
+        id: Date.now(),
+        hasFile: false,
+      };
+      setPhotoDetailsList((prevList) => [...prevList, newPhoto]);
+    }
+  };
 
-    return ( 
-        <div className='lg:w-1/2 overflow-y-auto flex flex-col justify-center items-center gap-5 p-4 lg:bg-camera-pattern lg:bg-contain'>
-            <PhotoDetails count={count} setCount={setCount}></PhotoDetails>
-            <AddPhotoButton count={count}></AddPhotoButton>
-        </div>
-     );
+  // Function to delete a PhotoDetails component by ID
+  const handleDeletePhoto = (id) => {
+    setPhotoDetailsList((prevList) => prevList.filter((photo) => photo.id !== id));
+  };
+
+  // Function to update whether a PhotoDetails component has a file
+  const updatePhotoHasFile = (id, hasFile) => {
+    setPhotoDetailsList((prevList) =>
+      prevList.map((photo) => (photo.id === id ? { ...photo, hasFile } : photo))
+    );
+  };
+
+  return (
+    <div className="lg:w-1/2 overflow-y-auto flex flex-col justify-center items-center gap-5 p-4 lg:bg-camera-pattern lg:bg-contain">
+      {photoDetailsList.map((photo, index) => (
+        <PhotoDetails
+          key={photo.id}
+          id={photo.id}
+          onDelete={() => handleDeletePhoto(photo.id)}
+          updateHasFile={(hasFile) => updatePhotoHasFile(photo.id, hasFile)}
+        />
+      ))}
+      {/* Only enable the button if the last PhotoDetails component has a file */}
+      <AddPhotoButton
+        onAddPhoto={handleAddPhoto}
+        disabled={!photoDetailsList[photoDetailsList.length - 1]?.hasFile}
+      />
+    </div>
+  );
 }
 
 export default Editor;
