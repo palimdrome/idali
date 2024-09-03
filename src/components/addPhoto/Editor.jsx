5eea64eacdacffb0520a1df62ea3224dba3fab34
Jon Robien Jinon <robien.jinon@gmail.com>
2024-09-03 14:14:04 +0800

    Fixed UI on mobile

--- src/components/addPhoto/Editor.jsx
+++ src/components/addPhoto/Editor.jsx
@@ -47,7 +47,7 @@ function Editor() {
   };
 
   return (
-    <div className="lg:w-1/2 overflow-y-auto flex flex-col justify-center items-center gap-5 p-4 lg:bg-camera-pattern lg:bg-contain">
+    <div className="lg:w-1/2 w-full overflow-y-auto flex flex-col justify-center items-center gap-5 p-4 lg:bg-camera-pattern lg:bg-contain">
       {photoDetailsList.map((photo, index) => (
         <PhotoDetails
           key={photo.id}
