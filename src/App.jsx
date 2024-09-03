23d19126d89600a01566f8b53664f80cbdea7cbe
Jon Robien Jinon <robien.jinon@gmail.com>
2024-09-03 15:43:29 +0800

    Updated background image fit on mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NavigationBar from "./components/NavigationBar";
 function App() {
 
   return (
-    <div className='flex flex-col items-center justify-between lg:items-start min-h-screen max-h-screen w-screen bg-camera-pattern bg-contain lg:bg-none'>
+    <div className='flex flex-col items-center justify-between lg:items-start min-h-screen max-h-screen w-screen bg-camera-pattern bg-cover lg:bg-none'>
       <NavigationBar />
 
       <div className="flex flex-grow overflow-y-auto w-full">
