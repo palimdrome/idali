184d44e5a3880a5fe8c98005144fec28657ed526
palimdrome <limuelle.dca@gmail.com>
2024-09-05 10:17:36 +0800

    Fixed layout of 1x1 images in the preview

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,33 +1,82 @@
 import React from 'react';
 
-function Document({ imgSrcs2x2, imgSrcs1x1 }) {
+function Document({ images }) {
+
+    // Restructures user input to get the image sources and the number of 2x2 and 1x1 for each image source
+    // Example: 2x2: [["src/image.png", 3]] (Meaning: 3 2x2 copies of image.png)
+    // Example: 1x1: [["src/image.png", 6]] (Meaning: 6 1x1 copies of image.png)
+    const imageSrcs = () => {
+        let collectionOf2x2 = [];
+        let collectionOf1x1 = [];
+        images.map((image) => {
+            if (image.numOf2x2 > 0 || image.numOf1x1 > 0) {
+                if (image.numOf2x2 > 0) {
+                    let foo = [image.url, image.numOf2x2];
+                    collectionOf2x2.push(foo);
+                };
+                if (image.numOf1x1 > 0) {
+                    let bar = [image.url, image.numOf1x1];
+                    collectionOf1x1.push(bar);
+                };
+            };
+        });
+        return [collectionOf2x2, collectionOf1x1];
+    };
+
+    // Dynamically creates image elements of 2x2 images
+    const images2x2 = [];
+    for (let i = 0; i < imageSrcs()[0].length; i++) {
+        for (let j = 0; j < imageSrcs()[0][i][1]; j++) {
+            images2x2.push(
+                <img 
+                    key={`image2x2-${i}-${j}`}
+                    src={imageSrcs()[0][i][0]}
+                    alt={`2x2 ID ${i}-${j}`}
+                    className='border border-black'
+                    style={{width: '48mm', height: '48mm', borderWidth: '0.0078125mm'}}
+                />
+            )
+        }
+    }
+
+    // Dynamically creates image elements of 1x1 images
+    const images1x1 = [];
+    for (let i = 0; i < imageSrcs()[1].length; i++) {
+        for (let j = 0; j < imageSrcs()[1][i][1]; j++) {
+            images1x1.push(
+                <img 
+                    key={`image1x1-${i}-${j}`}
+                    src={imageSrcs()[1][i][0]}
+                    alt={`2x2 ID ${i}-${j}`}
+                    className='border border-black'
+                    style={{width: '24mm', height: '24mm', borderWidth: '0.0078125mm'}}
+                />
+            )
+        }
+    };
+    
+    // Groups 1x1 images into 4 (square)
+    const images1x1By4 = [];
+    for (let i = 0; i < Math.ceil(images1x1.length / 4); i++) {
+        const innerElements = [];
+        let j = 0;
+        while (j < 4 && i * 4 + j < images1x1.length) {
+            innerElements.push(images1x1[j]);
+            j++;
+        };
+        images1x1By4.push(
+            <div key={`imageGroup1x1-${i}-${j}`} className='flex flex-wrap content-start' style={{width: '48mm', height: '48mm'}}>
+                {innerElements}
+            </div>
+        );
+    }
+
     return (
         <div className='relative w-1/2 min-h-screen flex flex-col items-center overflow-auto justify-center bg-gray-100 p-4 lg:block'
         style={{position: 'absolute', top: '-9999px', left:'-9999px'}}>
             <div id='document' className='flex flex-wrap content-start bg-white shadow-lg p-6' style={{width: '210mm', height: '297mm'}}>
-                {imgSrcs2x2.map((src, index) => (
-                    <img 
-                        key={index}
-                        src={src}
-                        alt={`2x2 ID ${index + 1}`}
-                        className='border border-black'
-                        style={{width: '48mm', height: '48mm', borderWidth: '0.0078125mm'}}
-                    />
-                ))}
-                <div
-                    className='flex flex-wrap content-start'
-                    style={{width: `${Math.ceil(imgSrcs1x1.length / 4) * 48}mm`, height: '48mm'}}
-                >
-                    {imgSrcs1x1.map((src, index) => (
-                        <img
-                            key={index}
-                            src={src}
-                            alt={`1x1 ID ${index + 1}`}
-                            className='border border-black'
-                            style={{width: '24mm', height: '24mm', borderWidth: '0.0078125mm'}}
-                        />
-                    ))}
-                </div>
+                {images2x2}
+                {images1x1By4}
             </div>
         </div>
      );
--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -1,31 +1,26 @@
 import { Footer, FooterCopyright, Button, Modal } from "flowbite-react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Document from "./Document";
 
-function Foot(props) {
+function Foot() {
 
     const [openModal, setOpenModal] = useState(false);
 
-    const docSize = "a4";
-    const docOrientation = "portrait";
-    const imgSrcs1x1 = [
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG"
-    ]
-    const imgSrcs2x2 = [
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG",
-        "src/assets/image-1.JPG"
-    ]
+    const preferences = {
+        docSize: "a4",
+        docOrientation: "portrait",
+        numOfCopies: 1,
+        images: [
+            {
+                id: 1,
+                url: "src/assets/image-1.JPG",
+                numOf1x1: 9,
+                numOf2x2: 3
+            }
+        ]
+    }
 
     const [imgData, setImgData] = useState("");
 
@@ -33,14 +28,10 @@ function Foot(props) {
         const input = document.getElementById('document');
         html2canvas(input, {scale: 2}).then(canvas => {
             const imgData = canvas.toDataURL('image/png'); // the whole canvas as image
-
-            console.log("This is the Image: ");
-            console.log(imgData);
-
             const pdf = new jsPDF({
-                orientation: docOrientation,
+                orientation: preferences.docOrientation,
                 unit: 'mm',
-                format: docSize
+                format: preferences.docSize
             });
 
             const pdfWidth = 210;
@@ -65,20 +56,21 @@ function Foot(props) {
         const input = document.getElementById('document');
         html2canvas(input, {scale: 2}).then(canvas => {
             const imgData = canvas.toDataURL('image/png'); // the whole canvas as image
-
-            console.log("This is the Image: ");
-            console.log(imgData);
-
             setImgData(imgData);
         });
-    }
+    };
+
+    const createDocument = () => {
+        useEffect(() => {
+            <Document images={preferences.images} />
+        }, [preferences]);
+    };
 
     return (
         <Footer className="relative bg-[#30323d] px-5 py-8 lg:py-4 m-0 rounded-none flex flex-row justify-center gap-3 w-full">
 
             {/* To be hidden from view but still accessible for getElementById */}
-            <Document imgSrcs2x2={imgSrcs2x2} imgSrcs1x1={imgSrcs1x1}/>
-
+            <Document images={preferences.images} />
             <Button size='md' className="bg-white text-[#30323d] w-1/2" onClick={() => setOpenModal(true)}>PREVIEW</Button>
             <Modal show={openModal} onClose={() => setOpenModal(false)}>
                 <Modal.Header>Preview document</Modal.Header>
